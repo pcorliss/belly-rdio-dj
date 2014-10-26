f57fe518262e5f8c7f88b6adb5662caa5f1b0777
Philip Corliss <pcorliss@50projects.com>
2014-10-26 18:43:57 -0500

    Connection lights

--- public/common.js
+++ public/common.js
@@ -3,7 +3,13 @@ $(function() {
     window.rdio = $('#api').rdio($.cookie('playback'));
     $('#api').bind('ready.rdio', function(event, userInfo) {
       console.log("Rdio Ready!");
-      //rdio.play('a997982');
+      if(window.location.href.indexOf('follower') == -1){
+        $('#track1_connect').addClass('connected');
+        $('.left > .controls > button').attr('disabled', false)
+        $('.left > .controls > input').attr('disabled', false)
+      } else {
+        
+      }
     });
   };
 
